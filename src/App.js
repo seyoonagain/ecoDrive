005776a04e8727232dd29a8ad1a21b60207db2c3
Séyoon Jeong <seyoonagain@gmail.com>
2024-09-23 23:51:18 +0900

    Set up React Query with QueryClient

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Outlet } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Header from './components/Header';
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <>
       <Header />
-      <section className='relative top-16 z-0'>
-        <Outlet />
-      </section>
+      <QueryClientProvider client={queryClient}>
+        <section className='relative top-16 z-0'>
+          <Outlet />
+        </section>
+      </QueryClientProvider>
     </>
   );
 }
